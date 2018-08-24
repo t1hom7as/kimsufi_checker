99460b2f656dde5c1f8bc6841f94a18e3186c43b
t1hom7as <41154665+t1hom7as@users.noreply.github.com>
2018-08-24 19:13:06 +0100

    Feature/rework (#2)
    * First major release
    
    * Changed OVHs API URL
    
    * Added some unit tests
    
    * Reworked the code

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(name='kimsufi-checker',
-      version='0.1.0',
+      version='1.0.0',
       description='Checks for kimsufi availability',
-      author='Tom Creasey',
+      author='t1hom7as',
       author_email='NA',
       keywords='server',
       license='Apache 2.0',
--- src/kimsufi_checker/kimi.py
+++ src/kimsufi_checker/kimi.py
@@ -19,7 +19,12 @@ SERVER_DICT = {
     'ks12': '1801sk23'
 }
 
-URL = 'http://ws.ovh.com/dedicated/r2/ws.dispatcher/getAvailability2'
+URL = 'https://www.ovh.com/engine/api/dedicated/server/availabilities?country=eu'
+TIME = 0
+
+
+class KimiException(Exception):
+    pass
 
 
 class Colours:
@@ -30,52 +35,66 @@ class Colours:
 
 
 class Checker:
-    def __init__(self):
+    def __init__(self, selection):
         self.server = False
         self.server_choice = False
+        self.selection = selection
 
-        parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
-        parser.add_argument('ks', help='Available options:\nks1\nks2\nks3\nks4\n'
-                            'ks5\nks6\nks7\nks8\nks9\nks10\nks11\nks12\n')
-        args = parser.parse_args()
-        self.selection = args.ks
+    def grab_data(self):
+        try:
+            r = requests.get(URL)
+            if r.status_code == 200:
+                data = r.json()
+                return data
+        except requests.RequestException:
+            raise KimiException(f'{Colours.yellow}Unable to connect to OVH{Colours.reset}')
+        except (ValueError, KeyError):
+            raise KimiException(f'{Colours.yellow}Invalid response from OVH{Colours.reset}')
 
-        if self.selection not in SERVER_DICT.keys():
-            sys.exit(f'{Colours.yellow}Invalid option{Colours.reset}')
+    def mas_data(self, data):
+        for x in data:
+            if self.server_choice in x['hardware']:
+                for y in x['datacenters']:
+                    if y['datacenter'] == 'default':
+                        continue
+                    if y['availability'] != 'unavailable':
+                        self.server = True
+                        print(f'{Colours.green}Server available in: {y["datacenter"]}{Colours.reset}')
+                        print(f'https://www.kimsufi.com/uk/order/kimsufi.xml?reference={self.server_choice}\n')  # noqa: E501¬
 
-    def main(self):    # noqa: C901
-        for k, v in SERVER_DICT.items():
-            if k == self.selection:
-                self.server_choice = v
-
-        if not self.server_choice:
-            sys.exit(f'{Colours.yellow}Invalid option{Colours.reset}')
+    def loop(self):    # noqa: C901
+        print(f'\nProbing every {TIME} seconds...\n')
+        try:
+            self.server_choice = SERVER_DICT[self.selection]
+        except KeyError:
+            raise KimiException('Invalid selection')
 
         while True:
-            try:
-                r = requests.get(URL)
-                if r.status_code == 200:
-                    data = r.json()
-                    output = data['answer']['availability']
-            except requests.RequestException:
-                sys.exit(f'{Colours.yellow}Unable to connect to OVH{Colours.reset}')
-            except (ValueError, KeyError):
-                sys.exit(f'{Colours.yellow}Inavlid response from OVH{Colours.reset}')
+            data = self.grab_data()
+            self.mas_data(data)
+
+            if not self.server:
+                print(f'{Colours.red}No servers available{Colours.reset} {time.ctime()}')
+                time.sleep(TIME)
             else:
-                for x in output:
-                    if self.server_choice == x['reference']:
-                        for y in x['zones']:
-                            zone = y['zone']
-                            if y['availability'] != 'unavailable':
-                                    self.server = True
-                                    print(f'{Colours.green}Server available in: {zone}{Colours.reset}')
-                                    print(f'https://www.kimsufi.com/uk/order/kimsufi.xml?reference={self.server_choice}\n')  # noqa: E501
-                if not self.server:
-                    print(f'{Colours.red}No servers available{Colours.reset} {time.ctime()}')
-                    time.sleep(20)
-                else:
-                    break
+                break
 
 
 def main():
-    Checker().main()
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('ks', help='Available options:\nks1\nks2\nks3\nks4\n'
+                        'ks5\nks6\nks7\nks8\nks9\nks10\nks11\nks12\n')
+    args = parser.parse_args()
+    selection = args.ks
+
+    if selection not in SERVER_DICT.keys():
+        sys.exit(f'{Colours.yellow}Invalid option{Colours.reset}')
+
+    checker = Checker(selection)
+
+    try:
+        checker.loop()
+    except KimiException as ex:
+        sys.exit(ex.args[0])
+    except KeyboardInterrupt:
+        sys.exit('\rBye...')
